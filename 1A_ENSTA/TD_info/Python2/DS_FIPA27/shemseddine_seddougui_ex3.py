381671a3c7aa474e3d48f0250376c4ff25ee1876
0p3r1 <s.seddougui@gemsvers.com>
2024-12-16 12:07:34 +0100

    correc
    Signed-off-by: 0p3r1 <s.seddougui@gemsvers.com>

--- 1A_ENSTA/TD_info/Python2/DS_FIPA27/shemseddine_seddougui_ex3.py
+++ 1A_ENSTA/TD_info/Python2/DS_FIPA27/shemseddine_seddougui_ex3.py
@@ -4,7 +4,9 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+
 img = np.load("tableau_ex3.npy")
+
 
 def preliminaires():
     plt.figure()
@@ -13,16 +15,20 @@ def preliminaires():
     plt.title("Préliminaires")
     plt.show()
 
+
 # preliminaires()
+
 
 def norme(A, B):
     return np.sqrt(A**2 + B**2)
 
+
 def nbpair(x):
-    if x%2 == 0:
+    if x % 2 == 0:
         return True
     else:
         return False
+
 
 def verifK(K):
     dim = np.shape(K)
@@ -34,12 +40,13 @@ def verifK(K):
     else:
         return False
 
+
 def convolution(K, A):
     result, racinecarre = verifK(K)
     if result == True:
         taille = racinecarre // 2
-        res = np.array([float(0) for i in range(len(A))])
-        indices = np.arange(-taille,taille+1,1)
+        res = np.zeros(A.shape)
+        indices = np.arange(-taille, taille + 1, 1)
         nb_l = np.shape(A)[0]
         nb_c = np.shape(A)[1]
 
@@ -50,15 +57,18 @@ def convolution(K, A):
                 res[i, j] = somme
         return res
 
+
 def question4():
     K = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
     verif = convolution(K, img)
-    verif = verif[:-1] # On supprime la dernière ligne
-    verif = verif[1:] # On supprime la première ligne
-    verif = verif[:, :-1] # On supprime la dernière colonne
+    verif = verif[:-1]  # On supprime la dernière ligne
+    verif = verif[1:]  # On supprime la première ligne
+    verif = verif[:, :-1]  # On supprime la dernière colonne
     verif = verif[:, 1:]  # On supprime la première colonne
 
+
 # question4()
+
 
 def question5():
     Gx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
@@ -71,5 +81,6 @@ def question5():
     plt.colorbar()
     plt.title("")
     plt.show()
+
 
 question5()
