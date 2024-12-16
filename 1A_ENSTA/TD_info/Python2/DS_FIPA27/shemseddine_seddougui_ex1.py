6497a46961a172d414aee4ed733278517b6f6d92
0p3r1 <s.seddougui@gemsvers.com>
2024-12-16 11:55:53 +0100

    push ds fipa 27 python2
    Signed-off-by: 0p3r1 <s.seddougui@gemsvers.com>

---
+++ 1A_ENSTA/TD_info/Python2/DS_FIPA27/shemseddine_seddougui_ex1.py
@@ -0,0 +1,50 @@
+# --------------------------------------------------
+# -------------- EXERCICE 1 // FIPA 27 -------------
+# --------------------------------------------------
+
+import numpy as np
+from numpy.random import randint as ri
+import matplotlib.pyplot as plt
+
+np.random.seed(42)
+A = ri(0, ri(100), (ri(30, 42), ri(10, 30)))
+
+def question1():
+    nblignescol_A = np.shape(A)
+    nbelements_A = np.size(A)
+
+    return print(f"Il y a {nblignescol_A[0]} lignes et {nblignescol_A[1]} colonnes, soit {nbelements_A} éléments.")
+
+# question1()
+
+def verif_question1():
+    nbelements_A = np.size(A)
+    lignes = np.shape(A)[0]
+    colonnes = np.shape(A)[1]
+
+    nbelements_verif = lignes * colonnes
+
+    return print(f"Il y a bien {nbelements_verif} éléments après avoir multipliés le nb de colonnes avec le nb de lignes.")
+
+# verif_question1()
+
+def question2():
+    x = A[5, :] # 6e ligne
+    y = A[8, :] # 9e ligne
+    plt.plot(x, y, label='')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('')
+    plt.show()
+
+# question2()
+
+def question3():
+    X = np.linspace(1, 10, np.shape(A)[1])
+    B = np.array([X[i] for i in range(len(X)) if i%2 == 0])
+    B_moy = np.mean(B)
+    q4 = B_moy/X
+
+    return print(X), print(B), print(B_moy), print(q4)
+
+question3()
---
+++ 1A_ENSTA/TD_info/Python2/DS_FIPA27/shemseddine_seddougui_ex2.py
@@ -0,0 +1,45 @@
+# --------------------------------------------------
+# -------------- EXERCICE 2 // FIPA 27 -------------
+# --------------------------------------------------
+
+import matplotlib.pyplot as plt
+import numpy as np
+tab = np.load("tableau_ex2.npy")
+
+def question1():
+    dim_tab = np.ndim(tab)
+    min_tab = np.min(tab)
+    max_tab = np.max(tab)
+
+    return print(f"Dimension du tableau : {dim_tab} \nMinimum : {min_tab} \nMaximum : {max_tab}")
+
+# question1()
+
+def gauss2d(x, y):
+    return np.exp(-((x**2 + y**2) / 2))
+
+def question2():
+    x = np.linspace(-1, 1, 1000)
+    y = np.linspace(-1, 1, 1000)
+    X, Y = np.meshgrid(x, y)
+    G2D = gauss2d(X, Y)
+
+    G2D_max = np.max(G2D)
+    G2D[G2D > 0.8 * G2D_max] = 0
+
+    return X, Y, G2D
+
+# question2()
+
+def question3():
+    X, Y, G2D = question2()
+
+    plt.figure()
+    plt.imshow(G2D, extent=[-1, 1, -1, 1], cmap="autumn")
+    plt.contour(X, Y, G2D)
+    plt.imshow(tab, extent=[-1, 1, -1, 1], cmap="viridis", zorder=2)
+    plt.colorbar()
+    plt.title("Question 3")
+    plt.show()
+
+question3()
