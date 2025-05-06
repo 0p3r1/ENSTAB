f1fc6efba147ac4d89da0c8456758c820ee0691f
0p3r1 <s.seddougui@gemsvers.com>
2025-05-06 16:01:00 +0200

    feat(): add exo 1
    Signed-off-by: 0p3r1 <s.seddougui@gemsvers.com>

---
+++ 1A_ENSTA/SA2/AlgorithmiqueFondamentale/Recursivite/exo1.py
@@ -0,0 +1,18 @@
+def f(n):
+    if n == 0:
+        return 1
+    else:
+        return n * f(n - 1)
+
+
+# ---------------------
+# QUESTION
+# ---------------------
+
+# Une fonction célèbre est écrite ici de façon récursive. Quel est le nom de la fonction f ?
+
+# ---------------------
+# REPONSE
+# ---------------------
+
+# C’est la factorielle : 𝑛!
