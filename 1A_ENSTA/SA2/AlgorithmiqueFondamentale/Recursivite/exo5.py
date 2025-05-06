a46f9a5db7a187ae57d7f2a567745e9fe5d5d8b6
0p3r1 <s.seddougui@gemsvers.com>
2025-05-06 16:37:48 +0200

    feat(): add exo 5
    Signed-off-by: 0p3r1 <s.seddougui@gemsvers.com>

---
+++ 1A_ENSTA/SA2/AlgorithmiqueFondamentale/Recursivite/exo5.py
@@ -0,0 +1,27 @@
+# Question 1
+
+# Écrire une fonction récursive fibo_malin(a, b, n) qui utilisera a et b comme accumulateurs pour calculer Fn en fonction Fn-1 et Fn-2, on décidera arbitrairement que a représente Fn-1 et b prendra le rôle de Fn-2. indice: l'appel récursif sera fibo_malin(a + b, a, ????).
+
+
+def fibo_malin(a, b, n):
+    if n == 0:
+        return b
+    else:
+        return fibo_malin(a + b, a, n - 1)
+
+
+if __name__ == "__main__":
+    print(fibo_malin(1, 0, 1))
+
+
+# Question 2
+
+# La récursivité simplifie-t-elle encore la formulation dans ce cas ?
+
+# Oui car elle évite les appels redondants et suit une logique simple et linéaire.
+
+# Question 3
+
+# Quel nom porte cette forme de récursivité ?
+
+# Récursivité terminale.
