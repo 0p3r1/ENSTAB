14eb8efa6b02b999d55f87f4e6c44a38ce7cb225
0p3r1 <s.seddougui@gemsvers.com>
2025-05-19 09:32:28 +0200

    feat(): add td1 c exo3
    Signed-off-by: 0p3r1 <s.seddougui@gemsvers.com>

---
+++ 1A_ENSTA/SA2/IntroductionLanguageC/TP1/exo3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int a = 10;
+    int b = 3;
+    int c = a / b;
+
+    printf("%d divisé par %d vaut %d\n", a, b, c);
+    return 0;
+}
