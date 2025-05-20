2bbb94cb20ce1d5af1d1a3d3b83ad030c302018f
0p3r1 <s.seddougui@gemsvers.com>
2025-05-20 13:46:30 +0200

    fix(): exo1 algo fonda
    Signed-off-by: 0p3r1 <s.seddougui@gemsvers.com>

--- 1A_ENSTA/SA2/AlgorithmiqueFondamentale/TD1/exo1.py
+++ 1A_ENSTA/SA2/AlgorithmiqueFondamentale/TD1/exo1.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 
-def init_tab_random(n=1000, m=1000, seed=4242):
+def init_tab_random(n=10, m=100, seed=4242):
     np.random.seed(seed)
     return np.random.randint(0, m, size=n)
 
 
 if __name__ == "__main__":
-    tab = init_tab_random()
+    tab = init_tab_random(1000, 1000)
     print(tab, "\nSomme :", sum(tab))
