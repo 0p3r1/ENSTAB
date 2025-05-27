a8273dc9c09112779c5c0dcfa72e446be0f63491
0p3r1 <s.seddougui@gemsvers.com>
2025-05-27 16:20:14 +0200

    feat(): add td2 tri comptage correction algo fonda
    Signed-off-by: 0p3r1 <s.seddougui@gemsvers.com>

--- 1A_ENSTA/SA2/AlgorithmiqueFondamentale/TD2/tricomptage.c
+++ 1A_ENSTA/SA2/AlgorithmiqueFondamentale/TD2/tricomptage.c
@@ -4,6 +4,33 @@
 
 #define ARRAY_SIZE 42
 
+void triParComptage(int tab[]) {
+   int borneSuperieure = 0;
+
+   for (int i = 0; i < ARRAY_SIZE; i++) {
+      if (tab[i] > borneSuperieure) {
+         borneSuperieure = tab[i];
+      }
+   }
+
+   int tabComptage[borneSuperieure + 1];
+   for (int i = 0; i <= borneSuperieure; i++) {
+      tabComptage[i] = 0;
+   }
+
+   for (int i = 0; i < ARRAY_SIZE; i++) {
+      tabComptage[tab[i]]++;
+   }
+
+   int cpt = 0;
+   for (int i = 0; i <= borneSuperieure; i++) {
+      for (int j = 0; j < tabComptage[i]; j++) {
+         tab[cpt] = i;
+         cpt++;
+      }
+   }
+}
+
 int main () {
    time_t t;
    int rand_array[ARRAY_SIZE];
@@ -13,6 +40,13 @@ int main () {
 
    for(int i = 0 ; i < ARRAY_SIZE ; i++ ) {
       rand_array[i] = rand() % 50;
+      printf("%d ", rand_array[i]);
+   }
+   printf("\n");
+
+   triParComptage(rand_array);
+
+   for (int i = 0; i < ARRAY_SIZE; i++) {
       printf("%d ", rand_array[i]);
    }
    printf("\n");
