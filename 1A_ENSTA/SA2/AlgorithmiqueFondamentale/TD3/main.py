f1c8902cc67728eb425157c77aca0e3848c48706
0p3r1 <s.seddougui@gemsvers.com>
2025-06-03 16:29:52 +0200

    refactor(): update unadded files
    Signed-off-by: 0p3r1 <s.seddougui@gemsvers.com>

---
+++ 1A_ENSTA/SA2/AlgorithmiqueFondamentale/TD3/main.py
@@ -0,0 +1,6 @@
+# ========================================================================
+# Étude du problème du sac à dos : Complexité et Algorithmes de résolution
+# ========================================================================
+
+# 1. Présentation et formalisation du problème
+
