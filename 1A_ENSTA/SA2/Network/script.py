0cf75fc9f0909b77906444341bcbb16aa5180616
0p3r1 <s.seddougui@gemsvers.com>
2025-06-03 16:28:26 +0200

    refactor(): update deleted files
    Signed-off-by: 0p3r1 <s.seddougui@gemsvers.com>

--- 1A_ENSTA/SA2/Network/script.py
+++
@@ -1,8 +0,0 @@
-from scapy.all import *
-
-response = sr1(IP(dst="8.8.8.8")/UDP()/DNS(rd=1, qd=DNSQR(qname="www.google.com")))
-
-response.show()
-
-if response and response.haslayer(DNS):
-    print("@IP de google.com :", response[DNS].an.rdata)
